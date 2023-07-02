ca4fa06938c6b7ae301ae7bd901991a31c0c4ad1
Fedor Mileev <fmileev@gmail.com>
2023-07-02 16:25:52 +0300

    starting a lang

--- python_scripts/Baza_constanti.py
+++ python_scripts/Baza_constanti.py
@@ -1,6 +1,6 @@
 '''List of constants for the project'''
 
-from os.path import split as filesplit
+from os.path import split as filesplit, abspath
 from Main import ABSOLUTEPATH
 
 CODE_CONFIG_LOGS: int = 5
@@ -16,18 +16,20 @@ PASSWORD_LEN_MIN: int = 7
 PASSWORD_LENLMAX: int = 15
 PASSWOED_RPEAT_CHAR_MAX: int = 3
 
+ABSPATH: str = abspath()
 PATH_DB: str = r'c:\Users\fmile\Desktop\BOT(Fedor)\database\python_scripts\DATABASE.txt'
 PATH_LOGS: str = r'c:\Users\fmile\Desktop\BOT(Fedor)\database\python_scripts\LOGS.txt'
 CONST_FILE_NAME: str = 'Baza_constanti.py'
 DERFILE, NAMEFILE = filesplit(ABSOLUTEPATH)
 NAMEDB: str = 'DATABASE.txt'
 NAMELOGS: str = 'LOGS.txt'
-POSITIVE: bool = True
+POSITIVE: bool = True                            
 NEGATIVE: bool = False
 POSITIVE_LIST: tuple = ('да', 'ага', '+', 'yes', 'yep', 'ya', 'ofcourse', 'y', 'д' )
 NEGATIVE_LIST: tuple = ('нет', 'не', '-', 'no', 'nope', 'nah', 'n', 'н' )
 EXIT: int = NEGATIVE
-UNKNOW: int = -1
+UNKNOWN: int = -1
+UNKNOWN_STR: str = ''
 DATA_BASE: dict = {
     'path': PATH_DB,
     'name': 'Database',
@@ -69,3 +71,18 @@ FEILDS_RECORDS: tuple = (
     'TEXT_NOTIFICATION',
 )
 SEPARATION: str = '\t' * 3
+
+KEY_LANG: str = UNKNOWN_STR
+
+LANG_TO_CHOUSE: tuple[str] = (
+    'Русский',
+    'English',
+    'Italiano',
+    'Español',
+    'Polski',
+    '中國人',
+    '日本',
+    'հայերեն',
+    'عرب',
+    'Deutsch',
+)
